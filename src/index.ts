088f42b6ee81e6b993dbbd5e09bce78ff02859f9
saiyed Rahil <rahilsaiyed1711@gmail.com>
2025-06-22 14:18:57 +0530

    added req

--- src/index.ts
+++ src/index.ts
@@ -33,7 +33,7 @@ app.use((req:Request, res: Response, next: NextFunction)=>{
 
 app.use('/api',apiRouter)
 
-app.get('/',(res:Response)=>{
+app.get('/',(req:Request, res:Response)=>{
     res.send('Api working')
 }
 )
